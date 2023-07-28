78eb464e0346314fe11682abd6891d87b518e0bc
yasin <yasin.mustafa@gmail.com>
2023-07-28 08:59:15 +0100

    ADJUST1-108 Change UAL enum to have ESCAPE as one category

--- src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/enums/UnlawfullyAtLargeType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/enums/UnlawfullyAtLargeType.kt
@@ -2,7 +2,7 @@ package uk.gov.justice.digital.hmpps.adjustments.api.enums
 
 enum class UnlawfullyAtLargeType {
   RECALL,
-  ESCAPES_ABSCONDS_ROTL_FAILURES,
+  ESCAPE,
   SENTENCED_IN_ABSENCE,
   RELEASE_IN_ERROR,
 }
