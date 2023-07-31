7f579d8862ffde131710dcf327edc1322237c3f1
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-07-31 11:10:02 +0100

    ADJUST1-112 explictly casting to a string

--- src/main/resources/migration/common/V5__replace_postgres_enum_cols.sql
+++ src/main/resources/migration/common/V5__replace_postgres_enum_cols.sql
@@ -1,11 +1,11 @@
 ALTER TABLE adjustment
-ALTER COLUMN source TYPE varchar(50);
+ALTER COLUMN source TYPE varchar(50) using source::text;
 
 ALTER TABLE adjustment_history
-ALTER COLUMN change_type TYPE varchar(50);
+ALTER COLUMN change_type TYPE varchar(50) using change_type::text;
 
 ALTER TABLE adjustment_history
-ALTER COLUMN change_source TYPE varchar(50);
+ALTER COLUMN change_source TYPE varchar(50) using change_source::text;
 
 DROP TYPE source_systems;
 DROP TYPE change_types;
