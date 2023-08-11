a752b9fb7aa60b6affc2548391b06ff551e2f2dc
yasin <yasin.mustafa@gmail.com>
2023-08-11 11:10:05 +0100

    ADJUST1-79 Lint fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/respository/AdjustmentRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/respository/AdjustmentRepository.kt
@@ -12,7 +12,13 @@ import java.util.UUID
 
 @Repository
 interface AdjustmentRepository : JpaRepository<Adjustment, UUID> {
-  @Query("SELECT a FROM Adjustment a WHERE a.person = :person AND a.deleted = false " +
-    "AND (a.fromDate IS NULL  OR a.fromDate >= :fromDate OR a.adjustmentType IN (:adjustmentTypes))")
-  fun findCurrentAdjustmentsByPerson(person: String, fromDate: LocalDate, adjustmentTypes: List<AdjustmentType>? = listOf(REMAND, TAGGED_BAIL)): List<Adjustment>
+  @Query(
+    "SELECT a FROM Adjustment a WHERE a.person = :person AND a.deleted = false " +
+      "AND (a.fromDate IS NULL  OR a.fromDate >= :fromDate OR a.adjustmentType IN (:adjustmentTypes))",
+  )
+  fun findCurrentAdjustmentsByPerson(
+    person: String,
+    fromDate: LocalDate,
+    adjustmentTypes: List<AdjustmentType>? = listOf(REMAND, TAGGED_BAIL),
+  ): List<Adjustment>
 }
