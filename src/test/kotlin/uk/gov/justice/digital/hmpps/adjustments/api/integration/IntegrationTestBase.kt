d10b57eedbc31cffd0a0af77a91caf9a77ee8449
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-09-29 16:38:10 +0100

    ADJUST1-210 Fixing roles.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/integration/IntegrationTestBase.kt
@@ -24,7 +24,7 @@ abstract class IntegrationTestBase {
 
   internal fun setAdjustmentsMaintainerAuth(
     user: String = "Test User",
-    roles: List<String> = listOf("ROLE_ADJUSTMENTS_MAINTAINER", "ROLE_RELEASE_DATE_CALCULATOR"),
+    roles: List<String> = listOf("ROLE_ADJUSTMENTS_MAINTAINER", "ROLE_RELEASE_DATES_CALCULATOR"),
   ): (HttpHeaders) -> Unit = jwtAuthHelper.setAuthorisation(user, roles)
   internal fun setViewAdjustmentsAuth(
     user: String = "Test User",
