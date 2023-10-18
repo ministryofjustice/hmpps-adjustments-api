4eb335cb5f8f6d748acbacbebebea1a19a802a65
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-10-18 15:45:44 +0100

    ADJUST1-231 using plugin for version updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.5.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.6.0"
   kotlin("plugin.spring") version "1.9.10"
   kotlin("plugin.jpa") version "1.9.10"
+  id("se.patrikerdes.use-latest-versions") version "0.2.18"
 }
 
 configurations {
@@ -22,34 +23,34 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-  implementation("io.hypersistence:hypersistence-utils-hibernate-60:3.3.2")
+  implementation("io.hypersistence:hypersistence-utils-hibernate-60:3.6.0")
 
   // Enable kotlin reflect
   implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.21")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.1.1")
 
-  implementation("org.springframework:spring-jms:5.3.26")
+  implementation("org.springframework:spring-jms:6.0.13")
 
   // Database dependencies
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:postgresql:42.6.0")
 
   // OpenAPI
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
   // Test dependencies
   testImplementation("org.awaitility:awaitility-kotlin")
-  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
+  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
-  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.12")
+  testImplementation("io.jsonwebtoken:jjwt:0.12.3")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.18")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
   testImplementation("com.h2database:h2")
-  testImplementation("org.testcontainers:localstack:1.18.3")
+  testImplementation("org.testcontainers:localstack:1.19.1")
 }
 
 java {
