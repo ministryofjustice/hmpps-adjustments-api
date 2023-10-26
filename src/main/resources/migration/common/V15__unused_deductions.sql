38f75c6d7ff7f8c973b7f24ab1ac470918fd7229
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-10-26 08:33:32 +0100

    ADJUST1-161 going with migrate approach.

--- src/main/resources/migration/common/V15__unused_deductions.sql
+++ src/main/resources/migration/common/V15__unused_deductions.sql
@@ -1,4 +1,4 @@
-ALTER TABLE adjustment ADD COLUMN effective_days integer NULL;
+ALTER TABLE adjustment ADD COLUMN effective_days integer NOT NULL;
 ALTER TABLE adjustment ALTER COLUMN days_calculated integer NULL;
 
 
