da3f34bd7624af4dba0582c6101066ac2644488d
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-10-26 08:52:40 +0100

    ADJUST1-161 Fixing script.

--- src/main/resources/migration/common/V15__unused_deductions.sql
+++ src/main/resources/migration/common/V15__unused_deductions.sql
@@ -1,4 +1,4 @@
 ALTER TABLE adjustment ADD COLUMN effective_days integer NOT NULL;
-ALTER TABLE adjustment ALTER COLUMN days_calculated integer NULL;
+ALTER TABLE adjustment ALTER COLUMN days_calculated DROP NOT NULL;
 
 
