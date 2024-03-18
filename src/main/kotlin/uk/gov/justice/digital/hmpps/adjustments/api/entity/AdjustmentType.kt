0c8fe519a22bdc461813ff9acb242ca9253ddbb3
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-03-18 12:49:34 +0000

    CMG-70 content change

--- src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/entity/AdjustmentType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/entity/AdjustmentType.kt
@@ -3,10 +3,10 @@ package uk.gov.justice.digital.hmpps.adjustments.api.entity
 enum class AdjustmentType(val text: String) {
   REMAND("Remand"),
   TAGGED_BAIL("Tagged bail"),
-  UNLAWFULLY_AT_LARGE("Unlawfully at large (UAL)"),
+  UNLAWFULLY_AT_LARGE("UAL (Unlawfully at large)"),
   LAWFULLY_AT_LARGE("Lawfully at large"),
-  ADDITIONAL_DAYS_AWARDED("Additional days awarded (ADA)"),
-  RESTORATION_OF_ADDITIONAL_DAYS_AWARDED("RADA (Restoration of added days)"),
+  ADDITIONAL_DAYS_AWARDED("ADA (Additional days awarded)"),
+  RESTORATION_OF_ADDITIONAL_DAYS_AWARDED("RADA (Restoration of additional days awarded)"),
   SPECIAL_REMISSION("Special remission"),
   UNUSED_DEDUCTIONS("Unused deductions"),
   ;
