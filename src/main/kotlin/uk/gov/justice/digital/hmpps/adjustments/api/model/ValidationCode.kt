bd61242a5883828a7c5a8073f2c3e7a7afbda171
Pavilion Sahota <159157649+pavilionsahota@users.noreply.github.com>
2024-04-11 12:16:44 +0100

    Removed date references (#142)
    Co-authored-by: Pavilion Sahota <pavilion.sahota@justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/model/ValidationCode.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/model/ValidationCode.kt
@@ -12,9 +12,9 @@ enum class ValidationCode(val message: String, val validationType: ValidationTyp
   RADA_DAYS_MUST_BE_POSTIVE("The number of days restored must entered."),
   UAL_FROM_DATE_NOT_NULL("You must enter a date for the first day of UAL."),
   UAL_TO_DATE_NOT_NULL("You must enter a date for the last day of UAL."),
-  UAL_FROM_DATE_AFTER_TO_DATE("The first day of unlawfully at large date must be before the last day of unlawfully at large date."),
+  UAL_FROM_DATE_AFTER_TO_DATE("The first day of unlawfully at large must be before the last day of unlawfully at large."),
   UAL_TYPE_NOT_NULL("You must select the type of UAL."),
-  UAL_FIRST_DATE_CANNOT_BE_FUTURE("The first day of unlawfully at large date must not be in the future"),
-  UAL_LAST_DATE_CANNOT_BE_FUTURE("The last day of unlawfully at large date must not be in the future"),
-  UAL_DATE_MUST_BE_AFTER_SENTENCE_DATE("The first day of unlawfully at large date must be on or after the start of the sentence, %s"),
+  UAL_FIRST_DATE_CANNOT_BE_FUTURE("The first day of unlawfully at large must not be in the future"),
+  UAL_LAST_DATE_CANNOT_BE_FUTURE("The last day of unlawfully at large must not be in the future"),
+  UAL_DATE_MUST_BE_AFTER_SENTENCE_DATE("The first day of unlawfully at large must be on or after the start of the sentence, %s"),
 }
