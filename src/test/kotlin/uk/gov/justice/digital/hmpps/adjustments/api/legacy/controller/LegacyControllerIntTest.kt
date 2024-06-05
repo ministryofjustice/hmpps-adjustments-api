57c6d030cfe79b0aae760ad5482580b11b83584e
Pavilion Sahota <pavilion.sahota@justice.gov.uk>
2024-06-05 16:29:41 +0100

    Updated int tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/legacy/controller/LegacyControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/legacy/controller/LegacyControllerIntTest.kt
@@ -255,7 +255,7 @@ class LegacyControllerIntTest : SqsIntegrationTestBase() {
       comment = "Created",
       active = false,
       bookingReleased = false,
-      agencyId = null,
+      agencyId = "LDS",
     )
   }
 }
