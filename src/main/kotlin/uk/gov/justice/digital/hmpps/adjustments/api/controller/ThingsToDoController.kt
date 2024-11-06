880a671added791580030c2645cdc09b4f01adba
yasin <yasin.mustafa@gmail.com>
2024-11-06 11:48:16 +0000

    fix import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/controller/ThingsToDoController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/controller/ThingsToDoController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import uk.gov.justice.digital.hmpps.adjustments.api.service.ThingsToDo
+import uk.gov.justice.digital.hmpps.adjustments.api.model.ThingsToDo
 import uk.gov.justice.digital.hmpps.adjustments.api.service.ThingsToDoService
 
 @RestController
