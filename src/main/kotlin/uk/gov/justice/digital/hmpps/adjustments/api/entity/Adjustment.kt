0c6fe65ffaf13f3c02143e3c7d4d73c283f7088a
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2024-12-19 14:14:34 +0000

    Fixing linting issues

--- src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/entity/Adjustment.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/entity/Adjustment.kt
@@ -9,12 +9,12 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.Id
 import jakarta.persistence.OneToMany
 import jakarta.persistence.OneToOne
 import jakarta.persistence.PrimaryKeyJoinColumn
 import jakarta.persistence.Table
-import jakarta.persistence.FetchType
 import jakarta.validation.constraints.NotNull
 import org.hibernate.annotations.Type
 import java.time.LocalDate
