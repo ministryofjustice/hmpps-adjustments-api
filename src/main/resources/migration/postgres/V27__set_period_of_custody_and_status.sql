de0f81c20a035623740e2fd6127fa4f5cc8b660e
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-01-03 11:51:26 +0000

    Remove long running script.

--- src/main/resources/migration/postgres/V27__set_period_of_custody_and_status.sql
+++
@@ -1,4 +0,0 @@
-UPDATE adjustment SET status = 'ACTIVE' WHERE status = 'INACTIVE' AND legacy_data->>'adjustmentActive' = 'true';
-
-UPDATE adjustment SET current_period_of_custody = true WHERE legacy_data->>'bookingActive' = 'true';
-UPDATE adjustment SET current_period_of_custody = false WHERE legacy_data->>'bookingActive' != 'true';
