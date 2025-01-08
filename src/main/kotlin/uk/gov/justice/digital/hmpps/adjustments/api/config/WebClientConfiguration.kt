cd6ba17367237f57d9d6260647e55d0dfeb8339b
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-01-08 10:57:48 +0000

    Fixing SQS timeout issues.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/config/WebClientConfiguration.kt
@@ -1,83 +1,81 @@
 package uk.gov.justice.digital.hmpps.adjustments.api.config
 
-import io.netty.channel.ChannelOption
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ExchangeFunction
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
-import reactor.netty.http.client.HttpClient
+import uk.gov.justice.hmpps.kotlin.auth.authorisedWebClient
+import java.time.Duration
 
 @Configuration
 class WebClientConfiguration(
   @Value("\${hmpps.auth.url}") private val oauthApiUrl: String,
   @Value("\${prison.api.url}") private val prisonApiUri: String,
-  @Value("\${prison.api.timeout-seconds:90}") private val prisonApiTimeoutSeconds: Int,
+  @Value("\${prison.api.timeout-seconds:90}") private val prisonApiTimeoutSeconds: Long,
   @Value("\${adjudications.api.url}") private val adjudicationsApiUri: String,
+  @Value("\${adjudications.api.timeout-seconds:90}") private val adjudicationsApiTimeoutSeconds: Long,
   @Value("\${calculate-release-dates.api.url}") private val calculateReleaseDatesApiUrl: String,
+  @Value("\${calculate-release-dates.api.timeout-seconds:90}") private val calculateReleaseDatesApiTimeoutSeconds: Long,
   @Value("\${remand-and-sentencing.api.url}") private val remandAndSentencingApiUrl: String,
+  @Value("\${remand-and-sentencing.api.timeout-seconds:90}") private val remandAndSentencingApiTimeoutSeconds: Long,
 ) {
 
   @Bean
-  fun prisonApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
-    val filter = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
-    filter.setDefaultClientRegistrationId("hmpps-api")
-    val httpClient: HttpClient = HttpClient.create()
-      .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1000 * prisonApiTimeoutSeconds)
-    return WebClient.builder()
-      .baseUrl(prisonApiUri)
-      .clientConnector(ReactorClientHttpConnector(httpClient))
-      .filter(filter)
-      .build()
+  fun prisonApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient {
+    return builder.authorisedWebClient(
+      authorizedClientManager = authorizedClientManager,
+      registrationId = "hmpps-api",
+      url = prisonApiUri,
+      timeout = Duration.ofSeconds(prisonApiTimeoutSeconds),
+    )
   }
 
   @Bean
-  fun calculateReleaseDatesApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
-    val filter = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
-    filter.setDefaultClientRegistrationId("hmpps-api")
-
-    return WebClient.builder()
-      .baseUrl(calculateReleaseDatesApiUrl)
-      .filter(filter)
-      .build()
+  fun calculateReleaseDatesApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient {
+    return builder.authorisedWebClient(
+      authorizedClientManager = authorizedClientManager,
+      registrationId = "hmpps-api",
+      url = calculateReleaseDatesApiUrl,
+      timeout = Duration.ofSeconds(calculateReleaseDatesApiTimeoutSeconds),
+    )
   }
 
   @Bean
-  fun remandAndSentencingApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
-    val filter = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
-    filter.setDefaultClientRegistrationId("hmpps-api")
-
-    return WebClient.builder()
-      .baseUrl(remandAndSentencingApiUrl)
-      .filter(filter)
-      .build()
+  fun remandAndSentencingApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient {
+    return builder.authorisedWebClient(
+      authorizedClientManager = authorizedClientManager,
+      registrationId = "hmpps-api",
+      url = remandAndSentencingApiUrl,
+      timeout = Duration.ofSeconds(remandAndSentencingApiTimeoutSeconds),
+    )
   }
 
   @Bean
-  fun adjudicationApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
+  fun adjudicationApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient {
     val size = 4 * 1024 * 1024
     val strategies = ExchangeStrategies.builder()
       .codecs { it.defaultCodecs().maxInMemorySize(size) }
       .build()
-    val filter = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
-    filter.setDefaultClientRegistrationId("hmpps-api")
-    return WebClient.builder()
-      .baseUrl(adjudicationsApiUri)
-      .filter(filter)
+    return builder
       .exchangeStrategies(strategies)
-      .build()
+      .authorisedWebClient(
+        authorizedClientManager = authorizedClientManager,
+        registrationId = "hmpps-api",
+        url = adjudicationsApiUri,
+        timeout = Duration.ofSeconds(adjudicationsApiTimeoutSeconds),
+      )
   }
+
   private fun addAuthHeaderFilterFunction(): ExchangeFilterFunction {
     return ExchangeFilterFunction { request: ClientRequest, next: ExchangeFunction ->
       val filtered = ClientRequest.from(request)
