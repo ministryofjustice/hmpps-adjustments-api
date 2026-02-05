a3ed4a58b891d33e87e57f0a2d068c58eaf2ad73
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2026-02-05 09:04:12 +0000

    ADJST-1527 Fix race condition in DPS -> NOMIS adjustments sync

--- src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/service/AdjustmentsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/service/AdjustmentsService.kt
@@ -1,6 +1,9 @@
 package uk.gov.justice.digital.hmpps.adjustments.api.service
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.support.TransactionSynchronization
+import org.springframework.transaction.support.TransactionSynchronizationManager
 import uk.gov.justice.digital.hmpps.adjustments.api.entity.AdjustmentSource
 import uk.gov.justice.digital.hmpps.adjustments.api.entity.AdjustmentStatus
 import uk.gov.justice.digital.hmpps.adjustments.api.model.AdjustmentDto
@@ -15,13 +18,23 @@ class AdjustmentsService(
   private val adjustmentsTransactionalService: AdjustmentsTransactionalService,
 ) {
 
-  fun create(adjustments: List<AdjustmentDto>): CreateResponseDto = adjustmentsTransactionalService.create(adjustments).also {
-    adjustmentsDomainEventService.create(it.adjustmentIds, adjustments[0].person, AdjustmentSource.DPS, adjustments[0].adjustmentType)
+  @Transactional
+  fun create(adjustments: List<AdjustmentDto>): CreateResponseDto = adjustmentsTransactionalService.create(adjustments).also { response ->
+    TransactionSynchronizationManager.registerSynchronization(object : TransactionSynchronization {
+      override fun afterCommit() {
+        adjustmentsDomainEventService.create(response.adjustmentIds, adjustments[0].person, AdjustmentSource.DPS, adjustments[0].adjustmentType)
+      }
+    })
   }
 
+  @Transactional
   fun updateEffectiveDays(adjustmentId: UUID, adjustment: AdjustmentEffectiveDaysDto) {
     adjustmentsTransactionalService.updateEffectiveDays(adjustmentId, adjustment).also {
-      adjustmentsDomainEventService.updateEffectiveDays(adjustmentId, adjustment.person, AdjustmentSource.DPS)
+      TransactionSynchronizationManager.registerSynchronization(object : TransactionSynchronization {
+        override fun afterCommit() {
+          adjustmentsDomainEventService.updateEffectiveDays(adjustmentId, adjustment.person, AdjustmentSource.DPS)
+        }
+      })
     }
   }
 
@@ -34,22 +47,37 @@ class AdjustmentsService(
     recallId: UUID? = null,
   ): List<AdjustmentDto> = adjustmentsTransactionalService.findCurrentAdjustments(person, status, currentPeriodOfCustody, recallId)
 
+  @Transactional
   fun update(adjustmentId: UUID, adjustment: AdjustmentDto) {
     adjustmentsTransactionalService.update(adjustmentId, adjustment).also {
-      adjustmentsDomainEventService.update(adjustmentId, adjustment.person, AdjustmentSource.DPS, adjustment.adjustmentType)
+      TransactionSynchronizationManager.registerSynchronization(object : TransactionSynchronization {
+        override fun afterCommit() {
+          adjustmentsDomainEventService.update(adjustmentId, adjustment.person, AdjustmentSource.DPS, adjustment.adjustmentType)
+        }
+      })
     }
   }
 
+  @Transactional
   fun delete(adjustmentId: UUID) {
     adjustmentsTransactionalService.get(adjustmentId).also {
       adjustmentsTransactionalService.delete(adjustmentId)
-      adjustmentsDomainEventService.delete(adjustmentId, it.person, AdjustmentSource.DPS, it.adjustmentType)
+      TransactionSynchronizationManager.registerSynchronization(object : TransactionSynchronization {
+        override fun afterCommit() {
+          adjustmentsDomainEventService.delete(adjustmentId, it.person, AdjustmentSource.DPS, it.adjustmentType)
+        }
+      })
     }
   }
 
+  @Transactional
   fun restore(adjustments: RestoreAdjustmentsDto) {
     adjustmentsTransactionalService.restore(adjustments).also {
-      adjustmentsDomainEventService.create(adjustments.ids, it[0].person, AdjustmentSource.DPS, it[0].adjustmentType)
+      TransactionSynchronizationManager.registerSynchronization(object : TransactionSynchronization {
+        override fun afterCommit() {
+          adjustmentsDomainEventService.create(adjustments.ids, it[0].person, AdjustmentSource.DPS, it[0].adjustmentType)
+        }
+      })
     }
   }
 }
