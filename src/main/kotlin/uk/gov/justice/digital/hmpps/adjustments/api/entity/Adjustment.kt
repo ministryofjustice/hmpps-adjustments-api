764f5b2c0a63f6f7c4d541b01e806dc4ad2e54dd
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2026-02-11 08:03:38 +0000

    Fix build issue

--- src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/entity/Adjustment.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/adjustments/api/entity/Adjustment.kt
@@ -8,6 +8,7 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.Id
 import jakarta.persistence.OneToMany
 import jakarta.persistence.OneToOne
